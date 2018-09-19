c8587daf7a83996dc068d8c395740b4a00383ea3
Le Cong Hau <anhlahau.hl@gmail.com>
2018-09-19 16:26:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 sudo docker run --rm -v $(pwd):/app composer/composer install
 sudo chmod -Rf 777 storage/
+sudo docker-compose down
 sudo docker-compose up -d
 sudo docker-compose exec app php artisan key:generate
 sudo docker-compose exec app php artisan cache:clear
 sudo docker-compose exec app php artisan make:controller MyController
+echo "Laravel should work by access http://0.0.0.0:8080"
